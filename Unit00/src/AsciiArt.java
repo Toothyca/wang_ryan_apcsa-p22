c9354066eeb74095647160fb2a86bf05c1edfb1a
Toothyca <Toothyca@gmail.com>
2019-02-01 10:23:00 -0800

    Added files for Unit0 and Unit1

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,46 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name Ryan Wang
+//Date 1/31/2019
+//Class AP CSA
+//Lab  0
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Ryan Wang \n\n" );
+		System.out.println("Human" );
+		System.out.println("\n\n" );
+
+		System.out.println("              _____          " );
+		System.out.println("             |_   _|         " );
+		System.out.println("             |  |  |         " );
+		System.out.println("             [-----]         " );
+		System.out.println("                ||           " );
+		System.out.println("           ------------      " );
+		System.out.println("         /| _________  |\\       " );
+		System.out.println("        / | _________  | \\       " );
+		System.out.println("       / /| _________  |\\ \\       " );
+		System.out.println("      / / | _________  | \\ \\     " );
+		System.out.println("      | | | _________  |  | |   " );
+		System.out.println("      |_| | _________  |  |_|   " );
+		System.out.println("      |_| | _________  |  |_|    " );
+		System.out.println("      /||\\   | |   | |   /||\\       " );
+		System.out.println("             | |   | |          " );
+		System.out.println("             |_|   |_|           " );
+		System.out.println("             |_|   |_|          " );
+		System.out.println("             | |   | |           " );
+		System.out.println("             | |   | |           " );
+		System.out.println("           __|_| __|_|           " );
+
+
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,33 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name Ryan Wang
+//Date 1/31/2019
+//Class AP CSA
+//Lab  0
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Ryan \t  1/31/2019 \n\n" );
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");		
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+		System.out.println("+++++++++++++++++++++++++");
+	}
+}
